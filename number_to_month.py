ed1fc1f0497ac349ab9617a0e5a1c57fece60cda
facundoestradaa <es.facundo.08@gmail.com>
2026-03-31 10:10:03 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,29 @@
-# Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+   """Given a number between 1-12, return the corresponding month"""
+   if month == 1:
+       return "enero"
+   elif month == 2:
+       return "febrero"
+   elif month == 3:
+       return "marzo"
+   elif month == 4:
+       return "abril"
+   elif month == 5:
+       return "mayo"
+   if month == 6:
+       return "junio"
+   if month == 7:
+       return "julio"
+   if month == 8:
+       return "agosto"
+   if month == 9:
+       return "septiembre"
+   if month == 10:
+       return "octubre"
+   if month == 11:
+       return "noviembre"
+   if month == 12:
+       return "diciembre"
+   else:
+       return "error"
+
